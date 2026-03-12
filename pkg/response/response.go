c83af7adf0370a3c76c3ccfbb9408a57f470e3bf
zlf <cx2411@sina.com>
2026-03-12 13:58:58 +0800

    response

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -12,8 +12,13 @@ type Response struct {
 	Data interface{} `json:"data,omitempty"`
 }
 
+var (
+	SuccessCode = status_code.Success
+	ErrorCode   = status_code.BadRequest
+)
+
 func Success(c *gin.Context, data interface{}) {
-	c.JSON(200, Response{
+	c.JSON(int(SuccessCode), Response{
 		Code: int(status_code.Success),
 		Msg:  status_code.Success.Message(),
 		Data: data,
@@ -21,7 +26,7 @@ func Success(c *gin.Context, data interface{}) {
 }
 
 func Error(c *gin.Context, msg string, data interface{}) {
-	c.JSON(400, Response{
+	c.JSON(int(ErrorCode), Response{
 		Code: int(status_code.BadRequest),
 		Msg:  msg,
 		Data: data,
