2076f3731685898b9372ad5144b23476a91cf797
“john <15191797+cx2411@users.noreply.github.com>
2026-05-06 17:31:03 +0800

    feat: add version package for framework version

---
+++ pkg/version/version.go
@@ -0,0 +1,9 @@
+package version
+
+// Version is the current version of the framework
+const Version = "0.0.1"
+
+// GetVersion returns the current version of the framework
+func GetVersion() string {
+	return Version
+}
